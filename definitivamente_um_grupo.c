bcbee9ac7eb3e9e0e6ffeae87b91f59fbf1e79e9
Tayssa Bezerra <tayssasilvabezerra2005@gmail.com>
2024-11-27 18:47:56 -0400

    Colque comentários no código

--- definitivamente_um_grupo.c
+++ definitivamente_um_grupo.c
@@ -1,3 +1,12 @@
+/***********************************
+ * 
+ * Hytalo Bassi, 
+ * Mariana Silva, 
+ * Tayssa Silva
+ * Trabalho 2
+ * Professor(a): Diego Rubert
+ * 
+ */
 #include <stdio.h>
 #include "stdarch.h"
 #include "alloc.h"
@@ -8,41 +17,70 @@
 #define MAX_OP 3
 #define MAX_LINE 128
 
+//Registradores
 Registrator* pc = NULL;
 Registrator* acc = NULL;
 Registrator* ext = NULL;
 Registrator* dat = NULL;
 
+/**
+ * @brief Estrutura que representa uma instrução 
+ */
 typedef struct _instr {
-  Operations op;
-  Registrator *reg1;
-  
-  int val1;
-  
-  Registrator *reg2;
- 
-  struct _instr *prev;
-  struct _instr *next;
+  Operations op;             ///< Tipo de operação 
+  Registrator *reg1;        ///< Primeiro registrador
+  int val1;                 ///< Valor inteiro associado à instrução
+  Registrator *reg2;        ///< Segundo registrador 
+  struct _instr *prev;      ///< Ponteiro para a instrução anterior
+  struct _instr *next;      ///< Ponteiro para a instrução seguinte
 } MetaInstruction;
 
+/**
+ * @brief Estrutura para armazena as instruções
+ */
 typedef struct queue {
-  MetaInstruction *head;
+  MetaInstruction *head;     ///< POnteiro para a primeira instrução
 } Queue;
 
+
+/**
+ * @brief Verifica se o registrador reg1 da instrução é NULL
+ * @param i A instrução a ser verificada
+ * @return Retorna 1 se reg1 for diferente de NULL
+ */
 int isRegister(MetaInstruction i) {
-  return i.reg1 != NULL;
+  return i.reg1 != NULL; // Retorna 1 se reg1 não for NULL, caso contrário, retorna 0
 }
 
+/**
+ * @brief Carrega um registrador baseado no nome passado
+ * @param op Nome do registrador (como "pc", "acc", etc)
+ * @return Retorna o ponteiro para o registrador correspondente ou NULL se não encontrado
+ */
 Registrator *loadRegSymbol(char *op) {
-  Registrator *reg = 
-    strcmp(op, "pc") == 0 ? pc :
-    strcmp(op, "acc") == 0 ? acc :
-    strcmp(op, "dat") == 0 ? dat :
-    strcmp(op, "ext") == 0 ? ext : NULL;
+  Registrator *reg = NULL; // Inicializa o ponteiro do registrador como NULL
+
+    if(strcmp(op, "pc") == 0)
+      reg = pc;
+    else if(strcmp(op, "acc") == 0)
+      reg = acc;
+    else if(strcmp(op, "dat") == 0)
+      reg = dat;
+    if else(strcmp(op, "ext") == 0) 
+      reg = ext;
 
   return reg;
 }
 
+/**
+ * @brief Criar uma nova instrução com os parâmetros fornecidos.
+ * 
+ * @param op Tipo da operação a ser realizada.
+ * @param reg1 Nome do primeiro registrador
+ * @param val1 Valor inteiro associado à operação
+ * @param reg2 Nome do segundo registrador
+ * @return REtorna uma nova instrução configurada
+ */
 MetaInstruction mkinstr(Operations op, char *reg1, int val1, char *reg2) {
   MetaInstruction instr; 
 
@@ -56,6 +94,12 @@ MetaInstruction mkinstr(Operations op, char *reg1, int val1, char *reg2) {
   return instr;
 }
 
+/**
+ * @brief Insere uma nova instrução na lista encadeada
+ * 
+ * @param head Ponteiro para início da lista de instruções
+ * @param i A instrução a ser inserida
+ */
 void insert(MetaInstruction *head, MetaInstruction *i) {
   MetaInstruction *ptr = head;
   if (ptr == NULL)
@@ -68,6 +112,11 @@ void insert(MetaInstruction *head, MetaInstruction *i) {
   i->prev = ptr;
 }
 
+/**
+ * @brief Executa a operação de acordo com tipo de instrução
+ * 
+ * @param i ponteiro para instrução a ser executada
+ */
 void load_op(MetaInstruction *i) {
   switch (i->op) {
       case ADD:
@@ -134,9 +183,8 @@ void load_op(MetaInstruction *i) {
         else
           mod(i->val1, acc);
       break;
-
   }
-
+  // Após a execução, a proxima instrução será chamada, se não for uma instrução de salto
   if (i->op != JMP &&
       i->op != JEQ &&
       i->op != JLT &&
@@ -144,6 +192,11 @@ void load_op(MetaInstruction *i) {
     jmp(1, pc);
 }
 
+/**
+ * @brief Carrega as instruções a partir da lista encadeada e executa cada uma.
+ * 
+ * @param first Ponteiro para a primeira instrução
+ */
 void load_instr(MetaInstruction *first) {
   int counter = 0;
   MetaInstruction *ptr = first;
@@ -151,6 +204,7 @@ void load_instr(MetaInstruction *first) {
   while (ptr != NULL) {
     load_op(ptr);
 
+    // Ajuda o ponteiro com base no valor do PC
     if (counter > *(pc->addr)) {
       for (size_t i = counter - *(pc->addr); i > 0; i--) {
         ptr = ptr->prev;
@@ -165,6 +219,9 @@ void load_instr(MetaInstruction *first) {
   } 
 }
 
+/**
+ * @brief Inicializa todos os registradores
+ */
 void init_all_reg() {
   pc = reg_alloc(PC);
   acc = reg_alloc(ACC);
@@ -177,6 +234,9 @@ void init_all_reg() {
   init_reg(ext, EXT); ///< Inicializa o registrador 'ext'.
 }
 
+/**
+ * @brief Libera a memória alocada para todos os registradores
+ */
 void free_all_reg() {
   free_reg(pc);   ///< Libera memória do registrador 'pc'.
   free_reg(acc);  ///< Libera memória do registrador 'acc'.
@@ -184,6 +244,11 @@ void free_all_reg() {
   free_reg(ext);  ///< Libera memória do registrador 'ext'.
 }
 
+/**
+ * @brief Libera a memória alocada para uma lista de instrução
+ * 
+ * @param n Ponteiro para a instrução a ser liberada
+ */
 void free_instr(MetaInstruction *n) {
   if (n != NULL && n->next != NULL)
     free_instr(n->next);
@@ -191,8 +256,14 @@ void free_instr(MetaInstruction *n) {
   free(n);
 }
 
+/**
+ * @brief Remove espaços em branco consecutivos de um string
+ * 
+ * @param string a ser processada
+ * @return Retorna a nova string sem espaços consecutivos
+ */
 char *rmxspace(char string[MAX_LINE + 1]) {
-   /* Variable Declaration and Initialization. */
+  /* DEclara a inicalização de variáveis.*/
   char *output = malloc(sizeof(char) * (MAX_LINE + 1));
   int c = 0, d = 0;
 
@@ -208,6 +279,11 @@ char *rmxspace(char string[MAX_LINE + 1]) {
   return output;
 }
 
+/**
+ * @brief Remove comentários de uma string
+ * 
+ * @param str A string a ser processada
+ */
 void rmcomment(char *str) {
   size_t len = strlen(str);
   size_t i = 0;
@@ -219,6 +295,11 @@ void rmcomment(char *str) {
     str[i] = 0;
 }
 
+/**
+ * @brief REmove espaços em brancos
+ * 
+ * @param str A string a ser processada
+ */
 void ltrim(char *str) {
     int i = 0;
 
@@ -234,6 +315,12 @@ void ltrim(char *str) {
     }
 }
 
+/** 
+ * @brief Normaliza a string removendo espaços, comentários e espaços
+ * 
+ * @param str A string a ser normalizada
+ * @return Retorna a string
+ */
 char *normalize_string(char str[MAX_LINE + 1]) {
   char *nStr = rmxspace(str);
   rmcomment(nStr);
@@ -242,6 +329,12 @@ char *normalize_string(char str[MAX_LINE + 1]) {
   return nStr;
 }
 
+/**
+ * @brief Carrega o tipo de opeação a partir de uma string
+ * 
+ * @param op A string representando o nome da operação.
+ * @return Retorna o tipo da operação correspondente
+ */
 Operations loadOpSym(char *op) {
   Operations nop = 
     strcmp(op, "add") == 0 ? ADD :
@@ -257,17 +350,25 @@ Operations loadOpSym(char *op) {
   return nop;
 }
 
-/// Importado de TayhBezerra
-/// @brief Função para identificar se uma string é um número inteiro
-/// @param ptr String a ser analisada
-/// @return Retorna 1 se a string for um número inteiro, caso contrário , retorna 0.
+/**
+ * Importado de TayhBezerra
+ *  @brief Função para identificar se uma string é um número inteiro.
+ * 
+ * @param ptr String a ser analisada
+ * @return Retorna 1 se a string for um número inteiro, caso contrário , retorna 0.
+ */
 int identifyIntegers(const char *ptr){
   char *endptr;
   strtol(ptr, &endptr, 10); //Tentar converter a string para inteiro.
   return *endptr == '\0'; //Verifica se todos os caracteres foram consumidos.
 }
 
-// @brief Recebe uma string normalizada e cria a instruction
+/**
+ * @brief Cria uma instrução a partir de um string normalizada
+ * 
+ * @param str A string normalizada contendo a instrução.
+ * @return Retorna o ponteiro para a instrução criada
+ */
 MetaInstruction *intprt(char *str) {
   MetaInstruction *i = malloc(sizeof(MetaInstruction));
   Operations op;
@@ -297,6 +398,12 @@ MetaInstruction *intprt(char *str) {
   return i;
 }
 
+/**
+ * @brief Insere uma instrução
+ * 
+ * @param q A fila de instruções
+ * i A instrução a ser inserida
+ */
 void insert_queue(Queue *q, MetaInstruction *i) {
   if (q->head == NULL)
     q->head = i;
@@ -304,7 +411,12 @@ void insert_queue(Queue *q, MetaInstruction *i) {
     insert(q->head, i);
 }
 
-// @brief Le um arquivo e retorna a cadeia de MetaInstruction;
+/**
+ * @brief Lê um arquivo e retorna uma fila de instruções
+ * 
+ * @param path Caminho do arquivo  aser lido
+ * @return Retorna uma fila de instruções carregadas do arquivo
+ */
 Queue *read_file(const char *path) {
   FILE *f = fopen(path, "r");
   
@@ -328,10 +440,12 @@ Queue *read_file(const char *path) {
   return q;
 }
 
-/// @brief Função principal do programa. Inicializa e libera registradores.
-/// @param argc Contagem de argumentos.
-/// @param argv Array de argumentos fornecidos pela linha de comando.
-/// @return Retorna `EXIT_SUCCESS` ao concluir com sucesso.
+/**
+ * @brief Função principal do programa. Inicializa e libera registradores.
+ * @param argc Contagem de argumentos.
+ * @param argv Array de argumentos fornecidos pela linha de comando.
+ * @return Retorna `EXIT_SUCCESS` ao concluir com sucesso.
+ */
 int main(int argc, char *argv[argc + 1]) {
   init_all_reg();
   
@@ -343,4 +457,3 @@ int main(int argc, char *argv[argc + 1]) {
   free_all_reg();
   return EXIT_SUCCESS;
 }
-
